d6d3c39b9f34630a480977b81ea254a88553eda9
DhanaSri-09 <151819805+DhanaSri-09@users.noreply.github.com>
2024-05-01 15:57:06 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,29 +1,56 @@
 import express from 'express';
-import { getAllCertificates, insertCertificate, updateCertificate, deleteCertificate } from './db.js';
+import cors from "cors";
+
+import { getAllCertificates, insertCertificate, updateCertificate, deleteCertificate, validateLogin, validateToken } from './db.js';
 
 const app = express();
 const Port = 3000;
 app.use(express.json());
+app.use(cors());
 
-app.get('/api/:employeeId/certs', async (req, res) => {
+
+app.post('/api/login', async(req, res) =>{
+    
+        const userName = req.body.userName;
+        const password = req.body.password;
+    try{
+        const response = await validateLogin(userName, password);
+        res.status(response.responseCode).send(response.data);
+    }
+    catch (error) {
+        res.status(500).send({ error: error.message });
+    }
+});
+
+app.use(async function(req, res, next){
+    let token = req.headers.token;
+    let result = await validateToken(token);
+    if (result)
+    {
+        req.employeeId = result;
+    }
+    next();
+});
+
+
+
+app.get('/api/certs', async (req, res) => {
     try {
-        const employeeId = req.params.employeeId;
+        const employeeId = req.employeeId;
         const sortBy = req.query.sortBy || 'certificateId'; 
         const sortOrder = req.query.sortOrder || 'ASC';
         const response = await getAllCertificates(employeeId, sortBy, sortOrder);
         res.status(response.responseCode).send(response.data);
-    } 
-    catch (error) 
-    {
+    } catch (error) {
         res.status(500).send({ error: error.message });
     }
 });
 
 
 
-app.post('/api/:employeeId/certs', async (req, res) => {
+app.post('/api/certs', async (req, res) => {
     try {
-        const employeeId = req.params.employeeId;
+        const employeeId = req.employeeId;
         const certificateData = req.body;
         const response = await insertCertificate(employeeId, certificateData);
         res.status(response.responseCode).send(response.data);
@@ -35,9 +62,9 @@ app.post('/api/:employeeId/certs', async (req, res) => {
 });
     
 
-app.put('/api/:employeeId/certs/:certificateId', async (req, res) => {
+app.put('/api/certs/:certificateId', async (req, res) => {
     try {
-        const employeeId = req.params.employeeId;
+        const employeeId = req.employeeId;
         const certificateId = req.params.certificateId;
         const certificateData = req.body;
         const response = await updateCertificate(employeeId, certificateId, certificateData);
@@ -50,10 +77,10 @@ app.put('/api/:employeeId/certs/:certificateId', async (req, res) => {
 });
 
 
-app.delete('/api/:employeeId/certs/:certificateId', async (req, res) => {
+app.delete('/api/certs/:certificateId', async (req, res) => {
     try 
     {
-        const employeeId = req.params.employeeId;
+        const employeeId = req.employeeId;
         const certificateId = req.params.certificateId;
         const response = await deleteCertificate(employeeId, certificateId);
         res.status(response.responseCode).send(response.data);
